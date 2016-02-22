692c95e2e8df58422056978de10dbe62d01ff7a2
Caleb <cfoust@sqweebloid.com>
2016-02-21 20:23:24 -0600

    Refactored StateController to be an inherited class.

---
+++ app/src/StateController.js
@@ -0,0 +1,54 @@
+var quat = quat || {};
+
+quat.StateController = Class.extend({
+	states: {},
+
+	init: function() {
+		var tempStates = {};
+		this.state = 0;
+
+		// Grab the user-defined states
+		var stateKeys = Object.keys(this.states);
+		for (var i = 0; i < stateKeys.length; i++) {
+			var key = stateKeys[i],
+			    obj = this.states[key];
+
+			if (typeof obj == "function") {
+				this[key] = function(o, n, self) {
+					return function() {
+						o(self,arguments);
+						self.setState(n);
+					};
+				}(obj, i, this);
+			} else {
+				this[key] = function(n, self) {
+					return function() {
+						self.setState(n);
+					};
+				}(i, this);
+			}
+			tempStates[key] = i;
+		};
+
+		this.states = tempStates;
+
+		this.developer = false;
+	},
+
+	stateToText: function(state) {
+        var keys = Object.keys(this.states);
+        return keys[state];
+    },
+
+    setDeveloper: function(enabled) {
+    	this.developer = enabled;
+    },
+
+	setState: function(state) {
+		if (this.developer) {
+            console.log(stateToText(this.state) + "->" + stateToText(state));
+        }
+		this.state = state;
+	}
+
+});
--- app/src/solver/SolverStateController.js
+++ app/src/solver/SolverStateController.js
@@ -1,114 +1,88 @@
 var quat = quat || {};
 quat.solver = quat.solver || {};
 
-/**
- * Handles all transitions between states.
- */
-quat.solver.SolverStateController = function(puzzleScene) {
-	// Grab all the references to things we need from puzzleScene
-	this.quatGame = puzzleScene.quatGame;
-	this.backgroundLayer = puzzleScene.backgroundLayer;
-    this.solutionLayer = puzzleScene.solutionLayer;
-    this.chooseLetterLayer = puzzleScene.chooseLetterLayer;
-    this.solutionSize = puzzleScene.solutionSize;
-
-    // The enumeration of all possible states for the solver GUI.
-    this.states = {
+quat.solver.SolverStateController = quat.StateController.extend({
+    states: {
         /*
         When only the current solution progress is showing.
          */
-        IDLE: 0,
+        IDLE: function(self) {
+            if ((self.state == self.states.CHANGING_LETTER_NODRAG) ||
+                (self.state == self.states.CHOOSING_LETTER)) {
+                self.chooseLetterLayer.setVisible(false);
+            }
+            self.solutionLayer.updateSolution(self.quatGame.getCurrentSteps());
+        },
         /*
         When the user clicked on one of the letters in the current word,
         but has not dragged out of the current word yet.
          */
-        CHOOSING_LETTER: 1,
+        CHOOSING_LETTER: function(self, arguments) {
+            var column = arguments[0];
+
+            // Move the letter chooser to its proper location
+            self.chooseLetterLayer.x = (column * (self.solutionSize.width / 4)) + self.solutionSize.x;
+            self.chooseLetterLayer.setBaseLetter(self.quatGame.getCurrentWord()[column]);
+            self.chooseLetterLayer.setVisible(true);
+        },
         /*
         The user has dragged out of the current word (selecting a new
         letter) and has stayed in a single column. The letter chooser is still
         tied to the mouse.
          */
-        CHANGING_LETTER_DRAG: 2,
+        CHANGING_LETTER_DRAG: null,
         /*
         The user has stopped dragging, but the letter chooser is still visible
         and is in the place they left it. They can either tap on the letter to
         enter it into the solution or drag again.
          */
-        CHANGING_LETTER_NODRAG: 3,
+        CHANGING_LETTER_NODRAG: null,
         /*
         The user started dragging in the solution space outside of the current
         word.
          */
-        GESTURING: 4,
+        GESTURING: null,
         /*
         The user has started swiping to the left to erase a word.
          */
-        ERASING_WORD: 5,
+        ERASING_WORD: null,
         /*
         The user has started swiping to the right to get to the main menu.
          */
-        SWIPING_TO_MENU: 6
-    };
+        SWIPING_TO_MENU: null
+    },
+
+    init: function(puzzleScene) {
+        this._super();
+        // Grab all the references to things we need from puzzleScene
+        this.quatGame = puzzleScene.quatGame;
+        this.backgroundLayer = puzzleScene.backgroundLayer;
+        this.solutionLayer = puzzleScene.solutionLayer;
+        this.chooseLetterLayer = puzzleScene.chooseLetterLayer;
+        this.solutionSize = puzzleScene.solutionSize;
 
 
-    // Sets the current state to IDLE
-    this.state = this.states.IDLE;
+        // Sets the current state to IDLE
+        this.state = this.states.IDLE;
 
-    // Setting this to true prints every state change
-    this.developer = false;
+        // Setting this to true prints every state change
+        this.developer = false;
 
-    var states = this.states;
-    var stateToText = function(state) {
-        var keys = Object.keys(states);
-        return keys[state];
+        var states = this.states;
+        var stateToText = function(state) {
+            var keys = Object.keys(states);
+            return keys[state];
+        }
+
+        this.setState = function(state) {
+            if (this.developer) {
+                console.log(stateToText(this.state) + "->" + stateToText(state));
+            }
+            this.state = state;
+        };
+
+        // Records the previous y offset of the last drag action
+        this.previousOffset = 0;
     }
 
-    this.setState = function(state) {
-        if (this.developer) {
-            console.log(stateToText(this.state) + "->" + stateToText(state));
-        }
-        this.state = state;
-    };
-
-    // Records the previous y offset of the last drag action
-    this.previousOffset = 0;
-}
-
-quat.solver.SolverStateController.prototype.IDLE = function() {
-	if ((this.state == this.states.CHANGING_LETTER_NODRAG) ||
-		(this.state == this.states.CHOOSING_LETTER)) {
-		this.chooseLetterLayer.setVisible(false);
-	}
-    this.solutionLayer.updateSolution(this.quatGame.getCurrentSteps());
-	this.setState(this.states.IDLE);
-};
-
-quat.solver.SolverStateController.prototype.CHOOSING_LETTER = function(column) {
-	// Move the letter chooser to its proper location
-	this.chooseLetterLayer.x = (column * (this.solutionSize.width / 4)) + this.solutionSize.x;
-    this.chooseLetterLayer.setBaseLetter(this.quatGame.getCurrentWord()[column]);
-    this.chooseLetterLayer.setVisible(true);
-
-	this.setState(this.states.CHOOSING_LETTER);
-};
-
-
-quat.solver.SolverStateController.prototype.CHANGING_LETTER_DRAG = function() {
-	this.setState(this.states.CHANGING_LETTER_DRAG);
-};
-
-quat.solver.SolverStateController.prototype.CHANGING_LETTER_NODRAG = function() {
-	this.setState(this.states.CHANGING_LETTER_NODRAG);
-};
-
-quat.solver.SolverStateController.prototype.GESTURING = function() {
-    this.setState(this.states.GESTURING);
-};
-
-quat.solver.SolverStateController.prototype.ERASING_WORD = function() {
-    this.setState(this.states.ERASING_WORD);
-};
-
-quat.solver.SolverStateController.prototype.SWIPING_TO_MENU = function() {
-    this.setState(this.states.SWIPING_TO_MENU);
-};
+});
